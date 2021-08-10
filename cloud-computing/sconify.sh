69f8c98411070b422a5ce99b84b3e730ba6c8d9a
maxence <maxence.cornaton@iex.ec>
2021-08-10 09:26:15 +0200

    Fix sconify.sh

--- cloud-computing/sconify.sh
+++ cloud-computing/sconify.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
+cd $(dirname $0)
+
+IMG_FROM=offchain-kaiko-pricefeed
+IMG_TO=offchain-tee-kaiko-pricefeed:debug
+
+ARGS=$(sed -e "s'\${IMG_FROM}'${IMG_FROM}'" -e "s'\${IMG_TO}'${IMG_TO}'" sconify.args)
+echo $ARGS
 
 docker run -it --rm \
             -v /var/run/docker.sock:/var/run/docker.sock \
             registry.scontain.com:5050/scone-production/iexec-sconify-image:5.3.7 \
-            sconify_iexec \
-                --name=offchain-tee-kaiko-pricefeed \
-                --from=offchain-tee-kaiko-pricefeed:temp-non-tee \
-                --to=offchain-tee-kaiko-pricefeed::tee-debug \
-                --binary-fs \
-                --fs-dir=/app \
-                --host-path=/etc/hosts \
-                --host-path=/etc/resolv.conf \
-                --binary="/usr/local/bin/python3.7" \
-                --heap="1G" \
-                --dlopen="2" \
-                --no-color \
-                --verbose \
-                --command="python /app/app.py"
+            sconify_iexec $ARGS
